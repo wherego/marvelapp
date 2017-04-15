2d4f083c88dc0b178589d891a6640ffa58320ad1
eric nguyen <enguyen8@googlemail.com>
2017-04-15 15:01:04 +0100

    adding useful libs, setting up modules for endpoints

---
+++ Marvel2/app/src/main/java/com/eric/airwick/marvel/Home/api/ApiModule.java
@@ -0,0 +1,16 @@
+package com.eric.airwick.marvel.Home.api;
+
+import javax.inject.Singleton;
+
+import dagger.Provides;
+
+/**
+ * Created by airwick on 15/04/2017.
+ */
+
+public class ApiModule {
+
+    //add services here?
+
+
+}
